b2b03e5efa2e85b44af981b71ac706ba6499c57c
SamBurgess93 <samburgess456@gmail.com>
2021-07-07 19:05:28 +0000

    added random color function and timeout

--- js/index.js
+++ js/index.js
@@ -1,11 +1,40 @@
 console.log("hey");
 
 let clickedCard = null;
+let preventClick = false;
+let combosFound = 0;
+
+
+const colors = [
+    "pink",
+    "yellow",
+    "green",
+    "red",
+    "blue",
+    "violet",
+    "orange",
+    "brown",
+]
+
+const cards = [...document.querySelectorAll(".card")];
+for (let color of colors) {
+    const cardAIndex = parseInt(Math.random() * cards.length);
+    const cardA = cards[cardAIndex];
+    cards.splice(cardAIndex, 1);
+    cardA.className += ` ${color}`;
+    cardA.setAttribute("data-color", color);
+
+    const cardBIndex = parseInt(Math.random()*cards.length);
+    const cardB = cards[cardBIndex];
+    cards.splice(cardBIndex, 1);
+    cardB.className += ` ${color}`;
+    cardB.setAttribute("data-color", color);
+}
 
 function onCardClicked(e){
     const target = e.currentTarget;
 
-    if (target === clickedCard || target.className.includes("done")) {
+    if (preventClick || target === clickedCard || target.className.includes("done")) {
         return;
     }
     target.className = target.className
@@ -16,12 +45,22 @@ function onCardClicked(e){
     if(!clickedCard) {
         clickedCard = target;       
     } else if (clickedCard){
-
-        if(clickedCard.getAttribute("data-color") === 
+        if(clickedCard.getAttribute("data-color") !== 
         target.getAttribute("data-color")){
-            console.log("cards are equal")
-        } else {
-            console.log("cards not equal")
+            preventClick = true;
+            setTimeout(()=>{
+                console.log("we are here");
+                clickedCard.className = clickedCard.className.replace("done", "").trim() + "color-hide";
+                target.className = target.className.replace("done", "").trim() + "color-hide";
+                clickedCard = null;
+                preventClick = false;
+            }, 500);
+        } else{
+        combosFound++;
+        clickedCard = null;
+        if (combosFound === 8){
+            alert("YOU WIN");
+        }
         }
     }
     
